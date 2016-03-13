6d87c740b52dcfbbc8df269f43d8c1769f103c55
Coloring <Coloring@coloringdemacbook-air.local>
2016-03-13 14:28:08 +0800

    Initial Commit

---
+++ CCPayPasswordView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCPayPasswordView
+//
+//  Created by Coloring on 16/3/13.
+//  Copyright © 2016年 Coloring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
